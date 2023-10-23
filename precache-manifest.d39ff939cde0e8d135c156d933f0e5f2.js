51f06d158e025284b6e465ecaee0be0ec68e4809
Aashu1303 <aashutoshag03@gmail.com>
2023-10-24 01:36:37 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.d39ff939cde0e8d135c156d933f0e5f2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c02149f41b03efa86887bfe69963614",
+    "url": "/aashutosh_portfolio/index.html"
+  },
+  {
+    "revision": "c9562efab949d6dd1141",
+    "url": "/aashutosh_portfolio/static/css/2.c95611d2.chunk.css"
+  },
+  {
+    "revision": "b56a01d096fbe247a3b8",
+    "url": "/aashutosh_portfolio/static/css/main.7f10ac14.chunk.css"
+  },
+  {
+    "revision": "c9562efab949d6dd1141",
+    "url": "/aashutosh_portfolio/static/js/2.1950281c.chunk.js"
+  },
+  {
+    "revision": "81a21ecbed68914c17dc",
+    "url": "/aashutosh_portfolio/static/js/3.4d967416.chunk.js"
+  },
+  {
+    "revision": "a333c69e977a4ea60745",
+    "url": "/aashutosh_portfolio/static/js/4.ba5cf164.chunk.js"
+  },
+  {
+    "revision": "b56a01d096fbe247a3b8",
+    "url": "/aashutosh_portfolio/static/js/main.4b9bb21f.chunk.js"
+  },
+  {
+    "revision": "843c1ecc9b521673e448",
+    "url": "/aashutosh_portfolio/static/js/runtime~main.781c68d9.js"
+  },
+  {
+    "revision": "a6452379835e910fa112916116fdd077",
+    "url": "/aashutosh_portfolio/static/media/Resume.a6452379.pdf"
+  },
+  {
+    "revision": "8fb0e5fd721a02afda190255b5ee649d",
+    "url": "/aashutosh_portfolio/static/media/about.aee0f771.png"
+  },
+  {
+    "revision": "6f37685cc33d46b55d34e3ddfbe5adf1",
+    "url": "/aashutosh_portfolio/static/media/avatar.6f37685c.svg"
+  },
+  {
+    "revision": "69948a28ccbd01fdd576340358088e37",
+    "url": "/aashutosh_portfolio/static/media/blog.fb8ab7f2.png"
+  },
+  {
+    "revision": "44190220743b70a49855d73399eec8d8",
+    "url": "/aashutosh_portfolio/static/media/chatify.7206044e.png"
+  },
+  {
+    "revision": "bc2c7afa280d7a7d54e2c5ecf515e8a6",
+    "url": "/aashutosh_portfolio/static/media/home-bg.dc52d5d4.jpg"
+  },
+  {
+    "revision": "1aed08b3e1b66991d921d25dbdcf28ed",
+    "url": "/aashutosh_portfolio/static/media/home-main.1aed08b3.svg"
+  },
+  {
+    "revision": "cf8c6935f68fde1543684d4a817f213a",
+    "url": "/aashutosh_portfolio/static/media/pre.cf8c6935.svg"
+  }
+]);
